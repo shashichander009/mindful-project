92151cf081bf7dad181f0d6433e9ed15c256d6ee
Preetam Singh <preetam.singh@mountblue.tech>
2020-09-14 13:05:40 +0530

    add auth guard

---
+++ mindful-ui/src/app/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  jwtHelper = new JwtHelperService();
+
+  constructor() { }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('access_token')!;
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+}
