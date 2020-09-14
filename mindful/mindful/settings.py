aa4910602e0b3020a3af88b56d06a0217d10aa8c
Rohit Sahoo <rohit.sahoo@mountblue.tech>
2020-09-14 15:27:05 +0530

    Add CORS Mechanism

--- mindful/mindful/settings.py
+++ mindful/mindful/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'mindful_api',
     'rest_framework',
     'rest_framework_swagger',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -97,6 +98,7 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
