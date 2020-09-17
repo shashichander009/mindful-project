0454ac0d7e02a1d44d485e0620194007df92f22e
Rohit Sahoo <rohit.sahoo@mountblue.tech>
2020-09-17 18:43:58 +0530

    Add second comment

--- mindful/mindful_api/serializers.py
+++ mindful/mindful_api/serializers.py
@@ -14,6 +14,7 @@ from .models import User, Post, Likes, Bookmarks, ReportPost
 UserModel = get_user_model()
 
 # comment added to test git workflow
+# comment second to test
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=False)
